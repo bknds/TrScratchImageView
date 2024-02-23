bbbbde3b4c1c18b70a943b4649af1ba7690273ed
青椒 <>
2024-02-23 16:31:47 +0800

    D

--- TrScratchImageView/TrScratchImageView.swift
+++ TrScratchImageView/TrScratchImageView.swift
@@ -35,7 +35,7 @@ public class TrScratchImageView: UIImageView {
     private var imageContext: CGContext!
     private var colorSpace: CGColorSpace!
     private var touchedPoints: [CGPoint]!
-    private let kSpotRadiusDefault: CGFloat = 45.0
+    private let kSpotRadiusDefault: CGFloat = 46.0
     private let kBezierStepFactor: CGFloat = 0.2
     
     // MARK: Private caculate properties
